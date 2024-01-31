87a4b877a3c92dce99e3ddfe3432936070ffcaf1
Domala Sherpa <domalasherpa9813@gmail.com>
2024-01-31 19:56:22 +0545

    add notes feature added

--- createData.js
+++ createData.js
@@ -2,42 +2,36 @@ import fileSystem from "fs";
 import inquirer from "inquirer";
 import { v4 as uuidv4 } from "uuid";
 import queryDB from "./queryDB.js";
+import writeToFile from "./writeToFile.js";
 
-let questions = [
-    { type:"input", name:"title", message:"Title: " },
-    { type:"input", name:"body", message: "Body: " }
-];
-
-let fileURN = "db.json";
-
+// ADD new data to the file
 export default async function createDate(info){
+    let questions = [
+        { type:"input", name:"title", message:"Title: " },
+        { type:"input", name:"body", message: "Body: " }
+    ];
     
+    let fileURN = "db.json";
+
     try{
         let answer = await inquirer.prompt(questions);
         info.push(answer);
-        console.log(info);
-        
+                
         if(!fileSystem.existsSync(fileURN)){
-            fileSystem.appendFile(fileURN, "", (error)=>{
+            fileSystem.appendFile(fileURN, "[]", (error)=>{
                 if(error){
                     console.log("Error while creating new file...", error);
                 }
             })
         }
-        addDetails(info);
+        await writeToFile(fileURN, info).then(
+            (value)=>{console.log("Data sucessfully added")},
+            (error)=>{console.log("Unable to add data.")}
+        );
     }
     catch(error){
         console.log("Something went wrong..", error);
     }
-}
-
-async function addDetails(info){
-    fileSystem.writeFile(fileURN, JSON.stringify(info), (error)=>{
-        if(error){
-            console.log(error);
-        }
-        console.log("Data added sucessfully");
-    })
 }
 
 queryDB(createDate);
