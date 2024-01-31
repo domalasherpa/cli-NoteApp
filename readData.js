84d3dca5d855e51cbbe685958b7be0a1e50a46a4
Domala Sherpa <domalasherpa9813@gmail.com>
2024-01-31 19:58:50 +0545

    readData output beautified

--- readData.js
+++ readData.js
@@ -1,3 +1,12 @@
 import queryDB from "./queryDB.js";
 
-queryDB();
+export default function readData(info){
+    info.forEach(note => {
+        console.log("\n");
+        for(let i in note){
+            console.log(`${i}: ${note[i]}`);
+        }
+    });
+}
+
+queryDB(readData);
